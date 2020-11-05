28cc08f9a751e693282d3bf5c19863ca9bfe7084
elbeto <elbeto80@hotmail.com>
2020-11-05 14:34:02 -0500

    Agregar plantilla AdminLTE y organizar vistas comunes

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>@yield('titulo_pagina')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/plantilla.css">
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    </head>
+    
+    <body class="hold-transition sidebar-mini">
+        <div id="app">
+            <div class="wrapper">
+    
+                @include('comun/cabecera')
+
+                @include('comun/menu_izquierda')
+
+                
+                @yield('contenido')
+
+                @include('comun/pie_pagina')
+
+            </div>
+        </div>
+    </body>
+
+    <!-- Todos los js integrados y compliados con webpack -->
+    <script src="./js/plantilla.js"></script>
+    <script src="./js/app.js"></script>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/plantilla/css/all.min.css',
+    'resources/plantilla/css/ionicons.min.css',
+    'resources/plantilla/css/adminlte.min.css',
+ ], 'public/css/plantilla.css').
+ scripts([
+    'resources/plantilla/js/jquery.min.js',
+    'resources/plantilla/js/bootstrap.bundle.min.js',
+    'resources/plantilla/js/adminlte.min.js',
+    'resources/plantilla/js/demo.js',
+ ], 'public/js/plantilla.js');
